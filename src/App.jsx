c8c2d0dd2cfdd3aa7a678fc2ccd2ec186485e12a
Gowtham G <gowthamguruvayurappan29@gmail.com>
2025-09-20 20:54:06 +0530

    Implement router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BottomNavigation from "./Components/BottomNavigation";
+import DashBoard from "./Pages/DashBoard";
+// import WarningLogs from "./Pages/WarningLogs"; // example extra page
+// import Profile from "./Pages/Profile";         // example extra page
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <Router>
+      <div className="pb-16"> {/* padding so content doesn't overlap BottomNavigation */}
+        <Routes>
+          <Route path="/" element={<DashBoard />} />
+          {/* <Route path="/warnings" element={<WarningLogs />} />
+          <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+      </div>
+
+      {/* Fixed bottom navigation */}
+      <div className="fixed bottom-0 left-0 right-0">
+        <BottomNavigation />
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
