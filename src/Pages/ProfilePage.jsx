e017a827c02b573e2762944a2afcd3272f5d4494
Gowtham G <gowthamguruvayurappan29@gmail.com>
2025-09-23 23:35:45 +0530

    UI upgradations

--- src/Pages/ProfilePage.jsx
+++ src/Pages/ProfilePage.jsx
@@ -1,9 +1,57 @@
 import ProfileComponent from "../Components/ProfileComponent";
+import LoginForm from "../Components/LoginForm";
+import OtpVerification from "../Components/OTPVerifyForm";
+import { fetchUser } from "../Api";
+import { useEffect, useState } from "react";
+import Loader from "../Components/Loader";
 
 function ProfilePage() {
+  const [loading, setLoading] = useState(false);
+
+  const [profilePageRefresh, setProfilePageRefresh] = useState(0);
+  const [user, setUser] = useState(null);
+
+  const getUser = async () => {
+    setLoading(true);
+    try {
+      const response = await fetchUser();
+      setUser(response.data);
+      setLoading(false);
+    } catch (err) {
+      setUser(null);
+      setLoading(false);
+      console.error("Failed to fetch user:", err);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+  }, [profilePageRefresh]);
+
   return (
-    <div >
-      <ProfileComponent />
+    <div className="fixed inset-0 flex items-center justify-center  overflow-hidden">
+      {loading && <Loader />}
+
+      <div className="w-full max-w-md relative z-10">
+        {!user ? (
+          <LoginForm
+            setProfilePageRefresh={setProfilePageRefresh}
+            setLoading={setLoading}
+          />
+        ) : user.verifiedStatus ? (
+          <ProfileComponent
+            user={user}
+            setProfilePageRefresh={setProfilePageRefresh}
+            setLoading={setLoading}
+          />
+        ) : (
+          <OtpVerification
+            phoneNumber={user.phoneNumber}
+            setProfilePageRefresh={setProfilePageRefresh}
+            setLoading={setLoading}
+          />
+        )}
+      </div>
     </div>
   );
 }
